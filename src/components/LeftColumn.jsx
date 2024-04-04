f2be7126a12ec5e5673869d6022b647bcdc2c42c
SMNoushan <noushan.md@gmail.com>
2024-04-05 00:20:33 +0600

    added left column-newsCard(home page)

--- src/components/LeftColumn.jsx
+++ src/components/LeftColumn.jsx
@@ -2,6 +2,32 @@ import { NavLink } from "react-router-dom";
 import useData from "../hooks/useData";
 import SectionHeader from "./shared/SectionHeader";
 import Spinner from "./shared/Spinner";
+import { CiCalendar } from "react-icons/ci";
+import newsBanner1 from "/assets/1.png";
+import newsBanner2 from "/assets/2.png";
+import newsBanner3 from "/assets/3.png";
+
+const newsCard = (imgUrl) => {
+  return (
+    <div className="card rounded-md space-y-4">
+      <figure>
+        <img src={imgUrl} alt="news-bannerImg" />
+      </figure>
+      <div className="card-body p-0">
+        <h2 className="card-title">
+          Bayern Slams Authorities Over Flight Delay to Club World Cup
+        </h2>
+        <div className="card-actions justify-flex items-center text-lg font-medium">
+          <p className="text-dark-02">Sports</p>
+          <div className="flex items-center gap-2 text-dark-04">
+            <CiCalendar />
+            <p>Jan 4, 2022</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const LeftColumn = () => {
   const { data, loading } = useData("/data/categories.json");
@@ -27,6 +53,11 @@ const LeftColumn = () => {
             </NavLink>
           ))
         )}
+      </div>
+      <div className="space-y-8">
+        {newsCard(newsBanner1)}
+        {newsCard(newsBanner2)}
+        {newsCard(newsBanner3)}
       </div>
     </>
   );
